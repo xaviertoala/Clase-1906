f9c59a3bcb6bafe4cb1bbe2b6c56fab846323595
xaviertoala <xaviertoala2006@gmail.com>
2025-06-19 15:10:28 -0500

    Clase 1906

---
+++ src/main/java/com/mycompany/practica1906/Practica1906.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1906;
+
+
+public class Practica1906 {
+
+    public static void main(String[] args) {
+        System.out.println("No tocar");
+        
+        
+    }
+}
